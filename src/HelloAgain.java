3525f740ad01252ee3b1ddfe8ad525d8431071de
fjordAreX <henryk.badalyan@gmail.com>
2022-06-02 18:57:11 +0300

    something

--- src/HelloAgain.java
+++ src/HelloAgain.java
@@ -1,6 +1,6 @@
 public class HelloAgain {
     public static void main(String[] args) {
-        System.out.println("commit src folder, not .java file through terminal");
+        System.out.println("smth");
     }
 }
 
