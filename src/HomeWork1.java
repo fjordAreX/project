c3bd09f4b23a85c27a82f867dbdd5a63fe07fa31
fjordAreX <henryk.badalyan@gmail.com>
2022-06-05 22:38:15 +0300

    not as good as I'd like it to be

---
+++ src/HomeWork1.java
@@ -0,0 +1,173 @@
+import java.util.Arrays;
+import java.util.Locale;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class HomeWork1 {
+    public static void main(String[] args) {
+        // Level 1
+        System.out.println("\t\t\tLevel 1:");
+
+        // Exercise 1
+        System.out.println("Exercise 1:");
+
+        String hi = "                Hello ";
+        String world = " WoRld!";
+        char newLine = '\n';
+        System.out.println((hi.trim() + world.toLowerCase() + newLine).repeat(3));
+        // or
+        String hiTrimmed = hi.trim();
+        String worldLowerCase = world.toLowerCase();
+        String result = hiTrimmed + worldLowerCase + newLine;
+        String repeatedResult = result.repeat(3);
+        System.out.println(repeatedResult);
+
+        // Exercise 2
+        System.out.println("Exercise 2:");
+
+        double height = 1.8;
+        double weight = 73.6;
+        double bmi = weight / (height * height); //Math.pow(height,2)
+        System.out.println(bmi);
+        double bmiRounded = Double.parseDouble(String.format(Locale.ENGLISH, "%.1f", bmi));
+        System.out.println(bmiRounded);
+
+        // Exercise 3
+        System.out.println("\nExercise 3:");
+
+        char[] arr = {'a', 'b', 'c', 'd', 'e'};
+        String firstCopyOfArray = String.valueOf(arr);
+        System.out.println(firstCopyOfArray);
+        arr[3] = 'h';
+        String secondCopyOfArray = String.valueOf(arr);
+        System.out.println(secondCopyOfArray);
+
+        //Level 2
+        System.out.println("\n\t\t\tLevel 2:");
+
+        // Exercise 1
+
+        System.out.println("Exercise 1:");
+        String number = "234";
+        String text = "some_text";
+        short numberParsed = Short.parseShort(number);
+        short textLength = (short) text.length();
+        short results = (short) (numberParsed + textLength);
+        System.out.println(results);
+
+        //Exercise 2
+
+        System.out.println("\nExercise 2:");
+        byte a = 3;
+        byte b = 5;
+        byte resulted = (byte) Math.pow((a + b), 2);
+        System.out.println(resulted);
+
+        //Exercise 3
+        System.out.println("\nExercise 3:");
+
+        int[] intArr1 = {1, 2, 5, 7, 10};
+        int[] intArr2 = {2, 3, 2, 17, 15};
+        int[] answer = new int[intArr1.length * 3];
+
+        System.arraycopy(intArr1, 0, answer, 0, intArr1.length);
+
+        for (int i = intArr1.length, c = 0; i < intArr1.length * 2; c++, i++)
+            answer[i] = intArr2[c];
+
+        for (int i = intArr1.length * 2, c = 0; i < answer.length; i++, c++)
+            answer[i] = intArr1[c] * intArr2[c];
+
+        System.out.println(Arrays.toString(answer));
+
+        //Exercise 4
+        System.out.println("\nExercise 4:");
+
+        String word = "Hello world!";
+        String wordUpperCase = word.toUpperCase();
+        String wordReplaced = wordUpperCase.replace("L", "R");
+        String wordLimited = wordReplaced.substring(0, 8);
+        System.out.println(wordLimited);
+        // or
+        String answ = word.toUpperCase().replace('L', 'R').substring(0, 8);
+        System.out.println(answ);
+
+
+
+
+
+                                                //Level 3
+        System.out.println("\n\t\t\tLevel 3:\n");
+
+        String before = "<client>(Какие то данные)<data>79991113344;test@yandex.ru;Иванов Иван Иванович</data></client>";
+        String after = masking(before);
+        System.out.println(after);
+    }
+
+    public static String masking(String text){
+        String info = infoSpace(text);
+        String answer;
+        if (info.isEmpty()) return text;
+        else {
+            String[] arrOfInfo = info.split(";");
+            for (int i = 0; i< arrOfInfo.length ; i++) {
+                try {
+                    if (arrOfInfo[i].contains("@")) arrOfInfo[i] = maskEmail(arrOfInfo[i]);
+                    else if (isDigit(arrOfInfo[i])) arrOfInfo[i] = maskNumber(arrOfInfo[i]);
+                    else arrOfInfo[i] = maskName(arrOfInfo[i]);
+                } catch (IllegalStateException ignored){
+                }
+            }
+            answer = String.join(";",arrOfInfo);
+        }
+        return text.replace(info,answer);
+    }
+    private static String infoSpace(String text){
+        int openingInfo = text.indexOf("<data>");
+        int closingInfo = text.lastIndexOf("</data>");
+        String info = text.substring(openingInfo, closingInfo);
+        info = info.replace("<data>","");
+        return info;
+    }
+
+    private static String maskEmail(String text){
+
+        String maskedEmail;
+        Pattern emailPattern = Pattern.compile("(\\w+)@(\\w+)\\.(com|ru)");
+        Matcher matcher = emailPattern.matcher(text);
+        String email = "";
+
+        while (matcher.find()) {
+            email = matcher.group();
+        }
+        String emailsSite = email.substring(email.lastIndexOf("@"),email.lastIndexOf("."));
+        String emaskedMailsSite = "*".repeat(emailsSite.length()-1);
+
+        maskedEmail = email.replaceAll("\\w@\\w+", "*@"+emaskedMailsSite);
+        return maskedEmail;
+
+    }
+    private static String maskNumber(String text){
+        return text.replaceAll(text.substring(4,7),"***");
+    }
+
+    private static String maskName(String text){
+        String [] arr = text.split(" ");
+
+        String surnamesMaskedLetters = "*".repeat(arr[0].length()-2);
+        arr[0] = arr[0].replaceAll(arr[0].substring(1,arr[0].length()-1),surnamesMaskedLetters);
+
+        if (arr.length ==3) arr[2] = arr[2].replaceAll(arr[2].substring(1),".");
+        return String.join(" ",arr);
+    }
+
+    private static boolean isDigit(String s) throws NumberFormatException {
+        try {
+            Long.parseLong(s);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+}
+
