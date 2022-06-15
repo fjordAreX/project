f04228a94e4305a82ede8f05e4952ccf4d9dd61f
fjordAreX <henryk.badalyan@gmail.com>
2022-06-15 20:51:39 +0300

    Homework2

--- src/Homework2/Expert/ExpertMain.java
+++ src/Homework2/Expert/ExpertMain.java
@@ -13,23 +13,28 @@ public class ExpertMain {
         Map<Integer, String[]> trafficData = new HashMap<>();
         Map<Integer, Integer> topPopular = new LinkedHashMap<>();
 
+        //Сортируем полученную информацию
         for (Map.Entry<Integer, Map<String, String[]>> entry : allData.entrySet()) {
             Map<String, String[]> traffic = entry.getValue();
 
             for (Map.Entry<String, String[]> entryTrafficData : traffic.entrySet()) {
 
                 if (entryTrafficData.getKey().equals("input")) {
+                    // регион и все въехавшие номера
                     trafficData.put(entry.getKey(), entryTrafficData.getValue());
+                    // регион и количество въехавших машин
                     topPopular.put(entry.getKey(), entryTrafficData.getValue().length);
                 }
             }
         }
-
+        // определяем ТОП-5
         Map<Integer, Integer> top5 = topPopular.entrySet().stream()
                 .sorted(Map.Entry.<Integer, Integer>comparingByValue().reversed()).limit(5)
                 .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (x, y) -> y, LinkedHashMap::new));
 
+        // Определяем из каких регионов въезжали чаще всего
         Map<Integer, Map<String, Integer>> popularIncomingRegions = new HashMap<>();
+        // специальные номера
         int specialCars = 0;
 
         for (Map.Entry<Integer, String[]> entry : trafficData.entrySet()) {
@@ -53,9 +58,9 @@ public class ExpertMain {
                                 count += 1;
                             }
                         }
-
                         fromWhereAndHowMany.put(matcher.group(3), count);
 
+                        // специальные номера
                         if (matcher.group(1).equals("М") && matcher.group(2).equals("АВ")) {
                             specialCars += 1;
                             System.out.println(specialCars);
@@ -63,6 +68,7 @@ public class ExpertMain {
                     }
                 }
 
+                // самый популярный номер, въезжающий в регион
                 Map<String, Integer> biggestInput = fromWhereAndHowMany.entrySet().stream()
                         .sorted(Map.Entry.<String, Integer>comparingByValue().reversed()).limit(1)
                         .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
@@ -71,13 +77,15 @@ public class ExpertMain {
             }
         }
 
+        // вывод
         for (Map.Entry<Integer, Integer> entry0 : top5.entrySet()) {
 
             for (Map.Entry<Integer, Map<String, Integer>> entry : popularIncomingRegions.entrySet()) {
 
+                if (entry0.getKey().equals(entry.getKey())) {
+
                 for (Map.Entry<String, Integer> entry1 : entry.getValue().entrySet()) {
 
-                    if (entry0.getKey().equals(entry.getKey())) {
                         System.out.println("В регион " + entry0.getKey() +
                                 " больше всего въехало машин с номерами " + entry1.getKey()
                                 + " в количестве " + entry1.getValue() + " машин");
